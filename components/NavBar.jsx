0cf1fe230f9aa90286f52374a820402bf3505c18
bra20019613 <brianlagranda@gmail.com>
2023-10-18 23:12:46 -0300

    fix: navbar animation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -65,7 +65,7 @@ export function NavBar() {
     if (scrollY === 0 || scrollDir === 'up') {
       return (
         <nav
-          className={`w-full md:w-3/4 md:mx-auto md:rounded-bl-xl fixed top-0 left-0 right-0 z-10 bg-black/40 backdrop-blur-sm`}
+          className={`w-full md:w-3/4 md:mx-auto md:rounded-bl-xl fixed top-0 left-0 right-0 z-10 bg-black/40 backdrop-blur-sm animate-curtainEnter`}
         >
           <div className='flex justify-between items-center h-16'>
             {/* LOGO */}
@@ -103,26 +103,52 @@ export function NavBar() {
           {/* Navigation Links */}
           <Transition
             show={navbar}
-            enter='transition-transform duration-300 transform ease-out'
-            enterFrom='translate-y-[-30%] opacity-0'
-            enterTo='translate-y-0 opacity-100'
-            leave='transition-transform duration-100 transform ease-in'
-            leaveFrom='translate-y-0 opacity-100'
-            leaveTo='translate-y-[30%] opacity-0'
+            enter='transition-opacity duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='transition-opacity duration-300'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
             <ul
-              className={`h-screen flex flex-col justify-center items-center pb-44 gap-4 text-white text-xl`}
+              className={`h-screen flex flex-col justify-center items-center pb-44 gap-4 text-white text-xl my-8 curtainEnter`}
             >
               {links.map(({ label, route }) => (
                 <li
                   key={route}
-                  className='flex py-4 md:px-6 border-b-2 w-32 justify-center hover:text-black hover:bg-transparent hover:border-black'
+                  className='flex py-3 md:px-6 border-b-2 w-32 justify-center hover:text-black hover:bg-transparent hover:border-black'
                 >
                   <Link href={route} onClick={() => setNavbar(!navbar)}>
                     {label}
                   </Link>
                 </li>
               ))}
+              <div className='flex gap-12 mt-6'>
+                <Link href=''>
+                  <Image
+                    src='/img/linkedin.svg'
+                    width='0'
+                    height='0'
+                    alt='Linkedin icon'
+                    className='w-6 h-auto'
+                  />
+                </Link>
+
+                <Image
+                  src='/img/github.svg'
+                  width='0'
+                  height='0'
+                  alt='Github icon'
+                  className='w-6 h-auto'
+                />
+                <Image
+                  src='/img/mail.svg'
+                  width='0'
+                  height='0'
+                  alt='Mail icon'
+                  className='w-6 h-auto'
+                />
+              </div>
             </ul>
           </Transition>
         </nav>
