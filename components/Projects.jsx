c6c25f56bb24eb7e29bba9a8469287db7c95f656
Briandvlpr <brianlagranda@gmail.com>
2023-11-27 16:40:28 +0000

    Feat: Added skills icons and links for projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,31 +1,52 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
+
+import bsIcon from '../public/img/icons/skills/bs.svg';
+import gitIcon from '../public/img/icons/skills/git.svg';
+import jsIcon from '../public/img/icons/skills/js.svg';
+import reactIcon from '../public/img/icons/skills/react.svg';
 
 const projects = [
   {
     id: crypto.randomUUID(),
-    title: 'Blog',
-    skills: ['❤', '🧡', '💛', '💚', '💙', '💜'],
+    title: 'Tecnological',
+    skills: [
+      [reactIcon, 'React'],
+      [jsIcon, 'JavaScript'],
+      [bsIcon, 'Bootstrap'],
+      [gitIcon, 'Git'],
+    ],
     img: '/img/projects/tecnological.png',
     alt: 'Ecommerce project Tecnological',
-    urlDeploy: 'urlDeploy',
-    urlGithub: 'urlGithub',
+    urlDeploy: 'https://tecnological.vercel.app/',
+    urlGithub: 'https://github.com/brianlagranda/tecnological',
   },
   {
     id: crypto.randomUUID(),
-    title: 'Tecnological',
-    skills: ['❤', '🧡', '💛', '💚'],
-    img: '/img/projects/tecnological.png',
-    alt: 'Ecommerce project Tecnological',
-    urlDeploy: 'urlDeploy',
-    urlGithub: 'urlGithub',
+    title: 'Calculator',
+    skills: [
+      [reactIcon, 'React'],
+      [jsIcon, 'JavaScript'],
+      [bsIcon, 'Bootstrap'],
+      [gitIcon, 'Git'],
+    ],
+    img: '/img/projects/calculator.png',
+    alt: 'Calculator App project',
+    urlDeploy: 'https://calculator-chi-opal.vercel.app/',
+    urlGithub: 'https://github.com/brianlagranda/calculator',
   },
   {
     id: crypto.randomUUID(),
     title: 'Landing Page',
-    skills: ['❤', '🧡', '💚', '💙', '💜'],
+    skills: [
+      [reactIcon, 'React'],
+      [jsIcon, 'JavaScript'],
+      [bsIcon, 'Bootstrap'],
+      [gitIcon, 'Git'],
+    ],
     img: '/img/projects/tecnological.png',
     alt: 'Ecommerce project Tecnological',
     urlDeploy: 'urlDeploy',
@@ -36,7 +57,7 @@ const projects = [
 export default function Projects() {
   const [activeProject, setActiveProject] = useState(null);
 
-  const handleCheckboxChange = id => {
+  const handleCheckboxChange = (id) => {
     if (activeProject === id) {
       setActiveProject(null);
     } else {
@@ -49,6 +70,7 @@ export default function Projects() {
       id='projects'
       className='w-full mt-24 flex flex-wrap gap-10 justify-center'
     >
+      <h2 className='m-auto text-5xl text-emerald-900'>Projects</h2>
       {projects.map(({ id, title, skills, img, alt, urlDeploy, urlGithub }) => (
         <label
           key={id}
@@ -56,7 +78,7 @@ export default function Projects() {
         >
           <input
             type='checkbox'
-            className=' hidden h-full'
+            className='hidden h-full'
             checked={activeProject === id}
             onChange={() => handleCheckboxChange(id)}
           />
@@ -64,26 +86,56 @@ export default function Projects() {
             <Image
               width={500}
               height={500}
-              style={{ objectFit: 'contain' }}
               className='rounded-lg ease-in-out duration-500 group-hover:rotate-6 group-hover:scale-125'
               src={img}
               alt={alt}
             />
             {activeProject === id && (
               <div className='absolute inset-0 flex flex-col items-center justify-center bg-black/60 hover:animate-curtainEnter'>
-                <h2 className='text-white text-lg font-semibold mb-4'>
+                <h2 className='text-white text-3xl font-semibold mb-4'>
                   {title}
                 </h2>
-                <div className='flex space-x-4'>
-                  <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
-                    Button 1
-                  </button>
-                  <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600'>
-                    Button 2
-                  </button>
+                <div className='flex gap-8 filter invert'>
+                  <div className='bg-black/60 p-1 rounded-full'>
+                    <Link href={urlGithub}>
+                      <Image
+                        src='/img/icons/github.svg'
+                        width='0'
+                        height='0'
+                        alt='Github icon'
+                        className='bg-white rounded-full p-1 w-12 h-auto hover:scale-125 hover:invert transition-all'
+                      />
+                    </Link>
+                  </div>
+                  <div className='bg-black/60 p-1 rounded-full'>
+                    <Link href={urlDeploy}>
+                      <Image
+                        src='/img/icons/link.svg'
+                        width='0'
+                        height='0'
+                        alt='Github icon'
+                        className='bg-white rounded-full p-1 w-12 h-12 hover:scale-125 hover:invert transition-all'
+                      />
+                    </Link>
+                  </div>
                 </div>
-                <div className='flex justify-center w-full content-center h-12 mt-8 items-center bg-black/40 backdrop-blur-sm'>
-                  {skills}
+                <div className='flex justify-center w-full content-center h-12 mt-3 items-center bg-black/40 backdrop-blur-sm gap-2'>
+                  {skills.map((skill, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className='w-8 h-8 overflow-hidden filter invert'
+                      >
+                        <Image
+                          priority
+                          key={index}
+                          src={skill[0]}
+                          alt={skill[1]}
+                          className='w-full h-full object-contain'
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             )}
@@ -93,4 +145,3 @@ export default function Projects() {
     </section>
   );
 }
-
